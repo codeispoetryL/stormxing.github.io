49804fe71ef4972d8060760baf2d837eda3f783e
stormxing <stormxing@foxmail.com>
2017-11-13 23:23:05 +0800

    Site updated: 2017-11-13 23:23:04

---
+++ bootstrap/108/Scores.js
@@ -0,0 +1,53 @@
+function totalScores() {
+    var scores = 0;
+
+    var value1 = document.getElementById("gap1").value;
+    if (value1 === '统一建模语言') {
+        scores += 10;
+    }
+
+    var value2 = document.getElementById("gap2").value;
+    if (value2 === '封装性继承性多态性') {
+        scores += 10;
+    }
+
+    var radio1 = document.getElementsByName("uml");
+    if (radio1[1].checked === true) {
+        scores += 10;
+    }
+
+    var radio2 = document.getElementsByName("java");
+    if (radio2[0].checked === true) {
+        scores += 10;
+    }
+
+    var checkbox1 = document.getElementsByName("granularity");
+    if (checkbox1[0].checked === true && checkbox1[1].checked === true && checkbox1[3].checked === true && checkbox1[2].checked === false) {
+        scores += 10;
+    }
+
+    var checkbox2 = document.getElementsByName("diagram");
+    if (checkbox2[0].checked === true && checkbox2[1].checked === true && checkbox2[2].checked === true && checkbox2[3].checked === false) {
+        scores += 10;
+    }
+
+    var judge1 = document.getElementsByName("judge1");
+    if (judge1[1].checked === true) {
+        scores += 10;
+    }
+
+    var judge2 = document.getElementsByName("judge2");
+    if (judge2[0].checked === true) {
+        scores += 10;
+    }
+
+    var area = document.getElementById("area").value;
+    if (area === '模型是对现实世界的简化和抽象，模型是对所研究的系统、过程、失误或概念的一种表达形式。' +
+        '可以是物理实体；也可以是某种图形；或者是一种教学表达式。') {
+        scores += 20;
+    }
+
+
+    alert("您的得分为：" + scores + "分！");
+}
+
